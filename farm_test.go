c04eb46c2f39c5ed21320fd399cbb96cf76d9039
John Allison <jrallison@gmail.com>
2014-09-01 15:27:13 -0500

    Test error handling

--- farm_test.go
+++ farm_test.go
@@ -1,6 +1,7 @@
 package farm
 
 import (
+	"errors"
 	"sort"
 	"testing"
 )
@@ -35,5 +36,140 @@ func TestRun(t *testing.T) {
 
 	if err := <-r.Errors; err != nil {
 		t.Errorf("Error returned from run: %v", err)
+	}
+}
+
+func TestGeneratorError(t *testing.T) {
+	r := Run(
+		10,
+		func(in chan<- interface{}) error {
+			return errors.New("whoops")
+		},
+		func(val interface{}) (interface{}, error) {
+			return nil, nil
+		},
+	)
+
+	var count int
+
+	for _ = range r.Results {
+		count += 1
+	}
+
+	if count > 0 {
+		t.Errorf("Unexpected results returned.")
+	}
+
+	err := <-r.Errors
+
+	if err == nil {
+		t.Errorf("Error not returned from generator")
+	}
+
+	if err.Error() != "whoops" {
+		t.Errorf("Wrong error message: want: %d found: %d", "whoops", err.Error())
+	}
+}
+
+func TestGeneratorPanic(t *testing.T) {
+	r := Run(
+		10,
+		func(in chan<- interface{}) error {
+			panic("whoops")
+		},
+		func(val interface{}) (interface{}, error) {
+			return nil, nil
+		},
+	)
+
+	for _ = range r.Results {
+	}
+
+	err := <-r.Errors
+
+	if err == nil {
+		t.Errorf("Error not returned from generator")
+	}
+
+	if msg := "generating work for farm paniced: whoops"; err.Error() != msg {
+		t.Errorf("Wrong error message: want: %d found: %d", msg, err.Error())
+	}
+}
+
+func TestWorkerError(t *testing.T) {
+	r := Run(
+		10,
+		func(in chan<- interface{}) error {
+			for i := 0; i < 20; i++ {
+				in <- i
+			}
+			return nil
+		},
+		func(val interface{}) (interface{}, error) {
+			return val.(int) * 2, errors.New("whoops")
+		},
+	)
+
+	var count int
+
+	for _ = range r.Results {
+		count += 1
+	}
+
+	if count > 0 {
+		t.Errorf("Unexpected results returned.")
+	}
+
+	errs := make([]error, 0, 10)
+
+	for err := range r.Errors {
+		errs = append(errs, err)
+	}
+
+	if len(errs) != 10 {
+		t.Errorf("Wrong number of errors returned: want: %d found: %d", 10, len(errs))
+	}
+
+	if errs[0].Error() != "whoops" {
+		t.Errorf("Wrong error message: want: %d found: %d", "whoops", errs[0].Error())
+	}
+}
+
+func TestWorkerPanic(t *testing.T) {
+	r := Run(
+		10,
+		func(in chan<- interface{}) error {
+			for i := 0; i < 20; i++ {
+				in <- i
+			}
+			return nil
+		},
+		func(val interface{}) (interface{}, error) {
+			panic("whoops")
+		},
+	)
+
+	var count int
+
+	for _ = range r.Results {
+		count += 1
+	}
+
+	if count > 0 {
+		t.Errorf("Unexpected results returned.")
+	}
+
+	errs := make([]error, 0, 10)
+
+	for err := range r.Errors {
+		errs = append(errs, err)
+	}
+
+	if len(errs) != 10 {
+		t.Errorf("Wrong number of errors returned: want: %d found: %d", 10, len(errs))
+	}
+
+	if msg := "processing work for farm paniced: whoops"; errs[0].Error() != msg {
+		t.Errorf("Wrong error message: want: %d found: %d", msg, errs[0].Error())
 	}
 }
